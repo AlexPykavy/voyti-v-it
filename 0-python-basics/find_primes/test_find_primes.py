07595595448e962c1970841091ce23c9b5fa5839
Tioma10 <xovrax97@mail.ru>
2023-03-19 20:34:02 +0300

    and update test_find before save

--- 0-python-basics/find_primes/test_find_primes.py
+++ 0-python-basics/find_primes/test_find_primes.py
@@ -3,3 +3,9 @@ from find_primes import find_primes
 
 def test_find_primes_with_5():
     assert find_primes(5) == [2, 3, 5]
+
+def test_find_primes_with_0():
+    assert find_primes(0) == []
+
+def test_find_primes_with_0():
+    assert find_primes(10) == [2, 3, 5, 7]
