57c10938e37d1e467812a364cadd5bfd0bea2c61
Tioma10 <xovrax97@mail.ru>
2023-03-19 20:32:33 +0300

    update find_primes

---
+++ 0-python-basics/find_primes/find_primes.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def find_primes(n: int) -> List[int]:
+    primes = []
+
+    for i in range (2, n+1):
+        for y in primes:
+            if i % y == 0:
+                break
+        else:
+            primes.append(i)
+
+
+    return (primes)
+print(find_primes(10))
---
+++ 0-python-basics/find_primes/test_find_primes.py
@@ -0,0 +1,5 @@
+from find_primes import find_primes
+
+
+def test_find_primes_with_5():
+    assert find_primes(5) == [2, 3, 5]
