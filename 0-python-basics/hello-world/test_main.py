30bebcce179d2739792ab7754cc7f6d2498c3d34
Alexander Pykavy <aleksandrpykavyj@gmail.com>
2023-03-19 09:50:33 +0100

    Create hello-world example

---
+++ 0-python-basics/hello-world/test_main.py
@@ -0,0 +1,9 @@
+from main import greeting
+
+
+def test_greeting_with_person():
+    assert greeting("John") == "Hello, John!"
+
+
+def test_greeting_without_person():
+    assert greeting() == "Hello, World!"
