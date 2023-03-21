4985ba32080cc0ae843f7a00cfebdf1dabb7e252
Tioma10 <xovrax97@mail.ru>
2023-03-21 16:14:08 +0300

    fixed markup

--- 0-python-basics/find_primes/test_find_primes.py
+++ 0-python-basics/find_primes/test_find_primes.py
@@ -4,8 +4,10 @@ from find_primes import find_primes
 def test_find_primes_with_5():
     assert find_primes(5) == [2, 3, 5]
 
+
 def test_find_primes_with_0():
     assert find_primes(0) == []
 
-def test_find_primes_with_0():
+
+def test_find_primes_with_10():
     assert find_primes(10) == [2, 3, 5, 7]
