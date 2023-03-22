6ebb0b504c0486de1fcba1c39267328eb3e193e7
Tioma10 <128286448+Tioma10@users.noreply.github.com>
2023-03-22 14:03:08 +0300

    del sanya.py and change  main to greeting (#7)
    * rename main to greeting and del sanya
    
    * rename main to greeting

--- 0-python-basics/find_primes/sanya.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-def find_primes_oneline(n: int) -> List[int]:
-    # 1. ternary operator
-    # 2. all() vs any()
-    # 3. list comprehension
-
-    return [i for i in range(2, n + 1) if not any(i % div == 0 for div in range(2, i))]
