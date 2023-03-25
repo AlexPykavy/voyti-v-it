a0940214c76c25f891243ef9760d27bff11b4c07
Tioma10 <xovrax97@mail.ru>
2023-03-25 11:23:18 +0300

    calculation function is made

---
+++ 0-python-basics/calculate/calculate.py
@@ -0,0 +1,18 @@
+import re
+
+
+def calculate(e: str) -> float:
+    g = []
+    nums = re.findall('[0-9]+', e)
+    for i in nums:
+        g.append(float(i))
+    a = float(g[0])
+    b = float(g[1])
+    if "+" in e:
+        return a+b
+    if "-" in e:
+        return a-b
+    if "*" in e:
+        return a*b
+    if "/" in e:
+        return a/b
