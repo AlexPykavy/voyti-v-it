2df974305410e684bbf6e892ed62d569c6e43aec
Tioma10 <xovrax97@mail.ru>
2023-03-27 13:35:32 +0300

    modernizate calculate

--- 0-python-basics/calculate/calculate.py
+++ 0-python-basics/calculate/calculate.py
@@ -1,18 +1,67 @@
-import re
-
-
+# flake8: noqa: C901
 def calculate(e: str) -> float:
-    g = []
-    nums = re.findall('[0-9]+', e)
-    for i in nums:
-        g.append(float(i))
-    a = float(g[0])
-    b = float(g[1])
-    if "+" in e:
-        return a+b
-    if "-" in e:
-        return a-b
-    if "*" in e:
-        return a*b
-    if "/" in e:
-        return a/b
+    g = e.split()
+
+    def precedence(op):
+
+        if op == '+' or op == '-':
+            return 1
+        elif op == '*' or op == '/':
+            return 2
+
+        return 0
+
+    def calc(a, b, op):
+
+        if op == '+':
+            return a + b
+        elif op == '-':
+            return a - b
+        elif op == '*':
+            return a * b
+        elif op == '/':
+            return round(a / b, 1)
+
+    def evaluate(g):
+        stack = []
+        ops = []
+        i = 0
+
+        while i < len(g):
+            if g[i] == '(':
+                ops.append(g[i])
+            elif g[i] not in '+-/*()':
+                stack.append(float(g[i]))
+            elif g[i] == ')':
+                while len(ops) != 0 and ops[-1] != '(':
+
+                    numb2 = stack.pop()
+                    numb1 = stack.pop()
+                    op = ops.pop()
+                    stack.append(calc(numb1, numb2, op))
+
+                ops.pop()
+            else:
+                while (len(ops) != 0 and precedence(ops[-1]) >= precedence(g[i])):
+
+                    numb2 = stack.pop()
+                    numb1 = stack.pop()
+                    op = ops.pop()
+
+                    stack.append(calc(numb1, numb2, op))
+
+                ops.append(g[i])
+
+            i += 1
+
+        while len(ops) != 0:
+
+            numb2 = stack.pop()
+            numb1 = stack.pop()
+            op = ops.pop()
+
+            stack.append(calc(numb1, numb2, op))
+
+        return stack[-1]
+
+    return evaluate(g)
--- 0-python-basics/calculate/test_calculate.py
+++ 0-python-basics/calculate/test_calculate.py
@@ -1,17 +1,5 @@
 from calculate import calculate
 
 
-def test_calculate__sum_4():
-    assert calculate("2+2") == 4.0
-
-
-def test_calculate_with_sub_10():
-    assert calculate("20-10") == 10.0
-
-
-def test_calculate_with_mult_100():
-    assert calculate("10*10") == 100.0
-
-
-def test_calculate_with_div_50():
-    assert calculate("500/10") == 50.0
+def test_calculate_():
+    assert calculate(" 2 * 2 - ( 2 + 1 )") == 1.0
