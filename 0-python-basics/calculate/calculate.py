4e090dc6a0664e4886611d5cf5950d14a59517b7
Tioma10 <xovrax97@mail.ru>
2023-04-07 13:05:38 +0300

    adding changes to a function calculate

--- 0-python-basics/calculate/calculate.py
+++ 0-python-basics/calculate/calculate.py
@@ -1,67 +1,69 @@
-# flake8: noqa: C901
+def precedence(op):
+
+    if op == '+' or op == '-':
+        return 1
+
+    if op == '*' or op == '/':
+        return 2
+
+    return 0
+
+
+def calc(a, b, op):
+
+    if op == '+':
+        return a + b
+
+    elif op == '-':
+        return a - b
+
+    elif op == '*':
+        return a * b
+
+    elif op == '/':
+        return round(a / b, 1)
+
+
 def calculate(e: str) -> float:
-    g = e.split()
+    list = e.split()
+    stack = []
+    ops = []
 
-    def precedence(op):
+    for tok in list:
+        if tok == '(':
+            ops.append(tok)
 
-        if op == '+' or op == '-':
-            return 1
-        elif op == '*' or op == '/':
-            return 2
+        elif tok == ')':
+            while ops and ops[-1] != '(':
 
-        return 0
+                numb2 = stack.pop()
+                numb1 = stack.pop()
+                op = ops.pop()
 
-    def calc(a, b, op):
+                stack.append(calc(numb1, numb2, op))
 
-        if op == '+':
-            return a + b
-        elif op == '-':
-            return a - b
-        elif op == '*':
-            return a * b
-        elif op == '/':
-            return round(a / b, 1)
+            ops.pop()
 
-    def evaluate(g):
-        stack = []
-        ops = []
-        i = 0
+        elif tok in '+-/*':
+            while ops and precedence(ops[-1]) >= precedence(tok):
 
-        while i < len(g):
-            if g[i] == '(':
-                ops.append(g[i])
-            elif g[i] not in '+-/*()':
-                stack.append(float(g[i]))
-            elif g[i] == ')':
-                while len(ops) != 0 and ops[-1] != '(':
+                numb2 = stack.pop()
+                numb1 = stack.pop()
+                op = ops.pop()
 
-                    numb2 = stack.pop()
-                    numb1 = stack.pop()
-                    op = ops.pop()
-                    stack.append(calc(numb1, numb2, op))
+                stack.append(calc(numb1, numb2, op))
 
-                ops.pop()
-            else:
-                while (len(ops) != 0 and precedence(ops[-1]) >= precedence(g[i])):
+            ops.append(tok)
 
-                    numb2 = stack.pop()
-                    numb1 = stack.pop()
-                    op = ops.pop()
+        else:
+            stack.append(float(tok))
 
-                    stack.append(calc(numb1, numb2, op))
+    while len(ops) != 0:
 
-                ops.append(g[i])
+        numb2 = stack.pop()
+        numb1 = stack.pop()
+        op = ops.pop()
 
-            i += 1
+        stack.append(calc(numb1, numb2, op))
 
-        while len(ops) != 0:
-
-            numb2 = stack.pop()
-            numb1 = stack.pop()
-            op = ops.pop()
-
-            stack.append(calc(numb1, numb2, op))
-
-        return stack[-1]
-
-    return evaluate(g)
+    return stack[-1]
--- 0-python-basics/calculate/test_calculate.py
+++ 0-python-basics/calculate/test_calculate.py
@@ -3,3 +3,7 @@ from calculate import calculate
 
 def test_calculate_():
     assert calculate(" 2 * 2 - ( 2 + 1 )") == 1.0
+
+
+def test_calculate_sum_4():
+    assert calculate("2 + 2") == 4
