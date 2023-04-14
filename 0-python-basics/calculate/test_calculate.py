3fd96828547eed9002211ee2f4558de675919498
Tioma10 <128286448+Tioma10@users.noreply.github.com>
2023-04-14 13:34:19 +0300

    modernized calc (#17)

--- 0-python-basics/calculate/test_calculate.py
+++ 0-python-basics/calculate/test_calculate.py
@@ -2,16 +2,16 @@ from calculate import calculate
 
 
 def test_calculate__sum_4():
-    assert calculate("2+2") == 4.0
+    assert calculate(" 2 + 2") == 4.0
 
 
-def test_calculate_with_sub_10():
-    assert calculate("20-10") == 10.0
+def test_calculate_2():
+    assert calculate("2 + 2 * 3 + ( 2 + 6 )") == 16.0
 
 
-def test_calculate_with_mult_100():
-    assert calculate("10*10") == 100.0
+def test_calculate_3():
+    assert calculate("( ( 2 + 2 ) + 2 ) * 3 * ( 2 + 6 )") == 144.0
 
 
-def test_calculate_with_div_50():
-    assert calculate("500/10") == 50.0
+def test_calculate_4():
+    assert calculate("2 / 4 * 3 * ( 6 + 2 )") == 12.0
